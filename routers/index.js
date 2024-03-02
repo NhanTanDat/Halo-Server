6cb731ed272b3ef4c79f95c53e3d2da065f0489b
nhantandat <nhandat040@gmail.com>
2024-03-02 23:23:13 +0700

    update src

--- routers/index.js
+++ routers/index.js
@@ -1,13 +1,9 @@
 const accountRouter = require('./account.router');
-const shoeRouter = require('./shoe.router');
-const cartRouter = require('./cart.router');
-const orderRouter = require('./order.router');
+
 
 module.exports = (app) => {
   app.use('/api/accounts', accountRouter);
-  app.use('/api/shoes', shoeRouter);
-  app.use('/api/carts', cartRouter);
-  app.use('/api/orders', orderRouter);
+
   
   app.use('/*', (req, res) => {
     res.json({
